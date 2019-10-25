0f0baed051a4ff18a856b3e9a99ed155432a56c6
Thomas Klijnsma <thomas.klijnsma@cern.ch>
2019-10-25 15:25:36 +0200

    added more package files

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup
 
 setup(
     name          = 'templatepypipackage',
-    version       = '0.1',
+    version       = '0.2',
     license       = 'BSD 3-Clause License',
     description   = 'Package to setup a basic PyPI package',
     url           = 'https://github.com/tklijnsma/templatepypipackage.git',
-    download_url  = 'https://github.com/tklijnsma/templatepypipackage/archive/v0_1.tar.gz',
+    download_url  = 'https://github.com/tklijnsma/templatepypipackage/archive/v0_2.tar.gz',
     author        = 'Thomas Klijnsma',
     author_email  = 'tklijnsm@gmail.com',
     packages      = ['templatepypipackage'],
     zip_safe      = False,
     tests_require = ['nose'],
     test_suite    = 'nose.collector',
+    include_package_data = True,
     scripts       = [
         'bin/setup-pypi-package'
         ],
