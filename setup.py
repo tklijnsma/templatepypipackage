7c98ddb067960ca429f9a8b651a19af98f5bb011
Thomas Klijnsma <thomas.klijnsma@cern.ch>
2019-10-25 15:37:37 +0200

    versioning on pypi accidentally broken, moving to 0.4

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name          = 'templatepypipackage',
-    version       = '0.2',
+    version       = '0.4',
     license       = 'BSD 3-Clause License',
     description   = 'Package to setup a basic PyPI package',
     url           = 'https://github.com/tklijnsma/templatepypipackage.git',
-    download_url  = 'https://github.com/tklijnsma/templatepypipackage/archive/v0_2.tar.gz',
+    download_url  = 'https://github.com/tklijnsma/templatepypipackage/archive/v0_4.tar.gz',
     author        = 'Thomas Klijnsma',
     author_email  = 'tklijnsm@gmail.com',
     packages      = ['templatepypipackage'],
--- templatepypipackage/__init__.py
+++ templatepypipackage/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__version__ = '0.4'
+
 from .logger import setup_logger
 logger = setup_logger()
 
